ef864487ea25db6aca8846e5e290ba61b58bba1a
muxik <lqjxm666@163.com>
2020-11-01 19:25:38 +0800

    首页 分页

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,11 +6,19 @@ class Index
 {
     public function index()
     {
-        $articles = model('Article')->select();
+        $articles = model('Article')
+            ->order('create_time', 'asc')
+            ->paginate(10);
         $categorys = model('Category')->select();
+
         return view()->assign([
             'articles' => $articles,
+            'page' => [
+                'last'=> $articles->lastPage(),
+                'current'  => $articles->currentPage(),
+            ],
             'categorys' => $categorys,
         ]);
     }
+
 }
