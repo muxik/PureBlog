153924aee88a315a9cc15a32ea3b953249fe3d58
muxik <lqjxm666@163.com>
2020-11-01 14:57:42 +0800

    Composer Vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'topthink/think',
+  ),
+  'versions' => 
+  array (
+    'topthink/think' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'topthink/think-captcha' => 
+    array (
+      'pretty_version' => 'v2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '54c8a51552f99ff9ea89ea9c272383a8f738ceee',
+    ),
+    'topthink/think-installer' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52f4b717a46fd00bccd47c5a9f10a5c56d29fcf9',
+    ),
+  ),
+);
